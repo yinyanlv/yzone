ea73e716ac2d81b5bd0f6891e015b8f038d6de3f
yinyanlv <yanlv.yin@gmail.com>
2017-12-26 18:04:08 +0800

    修改portal

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -149,6 +149,12 @@ module.exports = {
               // It enables caching results in ./node_modules/.cache/babel-loader/
               // directory for faster rebuilds.
               cacheDirectory: true,
+              plugins: [
+                ["import", {
+                  libraryName: 'antd',
+                  style: 'css'
+                }]
+              ]
             },
           },
           // "postcss" loader applies autoprefixer to our CSS.
